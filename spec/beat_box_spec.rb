fd46ca56247ee28e9d473a90cba95864814e5966
Nick Spencer <strangebrew202@gmail.com>
2023-07-07 14:37:57 -0600

    Add beat_box_spec.rb, initilize tests for BeatBox Object and #append tests

---
+++ spec/beat_box_spec.rb
@@ -0,0 +1,28 @@
+require './lib/beat_box'
+require './lib/node'
+require './lib/linked_list'
+
+describe BeatBox do
+  describe "#initialize" do
+    it "exists" do
+      bb = BeatBox.new
+
+      expect(bb).to be_a(BeatBox)
+      expect(bb.list).to be_a(LinkedList)
+      expect(bb.list.head).to eq(nil)
+    end
+  end
+
+  describe "#append" do
+    it "split string and append words as individual nodes to list"
+      bb = BeatBox.new
+
+      bb.append("deep doo ditt")
+      expect(bb.list.head.data).to eq("deep")
+      expect(bb.list.head.next_node.data).to eq("doo")
+
+      bb.append("woo hoo shu")
+      expect(bb.count).to eq(6)
+    end  
+  end
+end
