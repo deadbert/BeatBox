b453bc4cd53bb757e0dc31c9e90103a850d46c83
Nick Spencer <strangebrew202@gmail.com>
2023-07-10 12:34:18 -0600

    Adjust tab spacing to meet convention

--- lib/node.rb
+++ lib/node.rb
@@ -1,10 +1,10 @@
 class Node
-    attr_reader :data
-    attr_accessor :next_node
+  attr_reader :data
+  attr_accessor :next_node
 
-    #initialize a Node object with given data, and default nil next_node
-    def initialize(data)
-        @data = data
-        @next_node = nil
-    end
+  #initialize a Node object with given data, and default nil next_node
+  def initialize(data)
+    @data = data
+    @next_node = nil
+  end
 end
